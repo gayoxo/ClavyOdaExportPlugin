486556c663112f1962eb31d3fa3c4f8c7dbdaa94
Administrator <admin@local.host>
2015-01-23 17:54:23 +0100

    asdasd

--- ClavileñoOdaExport/src/fdi/ucm/server/exportparser/oda2/remote/Oda2SaveRemoteCollection.java
+++ ClavileñoOdaExport/src/fdi/ucm/server/exportparser/oda2/remote/Oda2SaveRemoteCollection.java
@@ -311,14 +311,14 @@ public class Oda2SaveRemoteCollection extends SaveCollection {
 	   ZipOutputStream zos = null;
 	   try
 	   {
-	      try
-	      {
-	         source = SOURCE_FOLDER.substring(SOURCE_FOLDER.lastIndexOf("\\") + 1, SOURCE_FOLDER.length());
-	      }
-	     catch (Exception e)
-	     {
-	        source = SOURCE_FOLDER;
-	     }
+//	      try
+//	      {
+//	         source = SOURCE_FOLDER.substring(SOURCE_FOLDER.lastIndexOf("\\") + 1, SOURCE_FOLDER.length());
+//	      }
+//	     catch (Exception e)
+//	     {
+//	        source = SOURCE_FOLDER;
+//	     }
 	     fos = new FileOutputStream(zipFile);
 	     zos = new ZipOutputStream(fos);
 
